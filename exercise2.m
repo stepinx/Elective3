da06059fb532aec7b21ef5ac2df05b969a5c8064
stepinx <103186639+stepinx@users.noreply.github.com>
2022-08-05 20:31:54 +0800

    Add files via upload

---
+++ exercise2.m
@@ -0,0 +1,15 @@
+pkg load image
+
+f=imread('fruits.png');
+imfinfo('fruits.png')
+imshow(f);
+# The data type of the original image is uint5
+
+j=imresize(f,0.5);
+imwrite(j,'fruits2.png');
+imfinfo('fruits2.png')
+figure(2),imshow(j);
+
+hsv_f=rgb2hsv(f);
+imwrite(hsv_f,'fruits3.png');
+figure(3),imshow(hsv_f);
